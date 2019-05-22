36efa8357bf757d8f4fc17205f3b8007263567af
EeeeBin <heycoolbin@gmail.com>
2019-05-22 20:53:52 +0800

    Update flash.sh

--- firmware_en/flash.sh
+++ firmware_en/flash.sh
@@ -28,8 +28,8 @@ fi
 
 esptool.py --chip esp32 --port /dev/${port} --baud 921600 --before default_reset --after no_reset write_flash -z \
 --flash_mode dio --flash_freq 80m --flash_size detect \
-0x1000 bootloader.bin \
-0xf000 phy_init_data.bin \
-0x10000 MicroPython.bin \
-0x8000 partitions_mpy.bin \
+0x1000 bootloader_0x1000.bin \
+0xf000 phy_init_data_0xf000.bin \
+0x10000 MicroPython_0x10000.bin \
+0x8000 partitions_mpy_0x8000.bin \
 0x170000 spiffs_image_0x170000.img
