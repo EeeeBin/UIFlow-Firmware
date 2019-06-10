19251890f0846ed3c5086dc3d81d25a4f77f883c
sakabin <heycoolbin@gmail.com>
2019-06-10 09:57:38 +0800

    Fix StickC download error

--- UpdateLog.md
+++ UpdateLog.md
@@ -1,5 +1,14 @@
-### v1.2.3
-* fix color unit
-* add stickC support
-* add p2p by apikey
-* add bala motor
+### v1.3.2
+
+* Add wifi auto reconnect
+* Add stickC support
+* Add button double press
+* Add IMU GRY
+* Add wait_ms, cpu ticks_ms get
+* Delete some imu block temporarily
+* Delete Stick support
+* SD card will mount in startup
+* Fix bugs that ampy can't use
+* V1.2.3 version save in http://flow.m5stack.com/old
+
+* note: If appear out of pwm channels error, please reboot
--- firmware_en/flash.sh
+++ firmware_en/flash.sh
@@ -24,12 +24,9 @@ if [ "${machine}" == "MacOS" ]; then
     port="tty.SLAB_USBtoUART"
 fi
 
-# echo ${port}
-
 esptool.py --chip esp32 --port /dev/${port} --baud 921600 --before default_reset --after no_reset write_flash -z \
 --flash_mode dio --flash_freq 80m --flash_size detect \
 0x1000 bootloader_0x1000.bin \
-0xf000 phy_init_data_0xf000.bin \
-0x10000 MicroPython_0x10000.bin \
-0x8000 partitions_mpy_0x8000.bin \
-0x170000 spiffs_image_0x170000.img
+0x8000 partitions_0x8000.bin \
+0x10000 application_0x10000.bin \
+0x1F0000 fatfsImg_0x1f0000.img
